5ba13485787f7bc925b843daeb21cccfd2e22087
Shane <sav08@my.fsu.edu>
2017-12-04 13:46:35 +0000

    cleaned homepage

--- app.R
+++ app.R
@@ -4,16 +4,6 @@ library(sendmailR)###
 ui <- fluidPage(includeHTML('index.html'))
 
 server <- function(input, output, session){
-    
-    observe({
-      if(is.null(input$send) || input$send==0) return(NULL)
-      from <- isolate(input$from)
-      to <- isolate("shaneav2749@gmail.com")
-      subject <- isolate(input$subject)
-      msg <- isolate(input$message)
-      sendmail(from, to, subject, msg)
-    })
-    
   
 }
 
